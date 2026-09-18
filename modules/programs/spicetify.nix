eb0a0c1998688d57ce0527d3f907acb690b0eb77
shizukutakahashi55-del <shizukutakahashi55@gmail.com>
2026-09-17 19:48:50 -0700

    spotify fixes

--- modules/programs/spicetify.nix
+++ modules/programs/spicetify.nix
@@ -3,14 +3,14 @@
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
 
-  # Sleek vive dentro del monorepo oficial de temas de spicetify,
-  # así que traemos todo el repo y apuntamos solo a la subcarpeta "Sleek".
+  # Sleek vive dentro del monorepo oficial de temas de Spicetify.
   spicetifyThemesSrc = pkgs.fetchFromGitHub {
     owner = "spicetify";
     repo = "spicetify-themes";
-    rev = "master"; # cuando quieras reproducibilidad, cámbialo por un commit fijo
+    rev = "master";
     hash = "sha256-R0lho0h+GxAiHARFz31ZEJil5KSTR1/M0mFR/wnxK3o=";
   };
+
 in
 {
   imports = [
@@ -20,24 +20,17 @@ in
   programs.spicetify = {
     enable = true;
 
-    # Antes: theme = spicePkgs.themes.catppuccin; colorScheme = "mocha";
-    # Sleek no viene precargado en spicePkgs, así que lo definimos como tema custom.
     theme = {
       name = "Sleek";
       src = "${spicetifyThemesSrc}/Sleek";
-
       injectCss = true;
-      injectThemeJs = false; # Sleek no trae theme.js; revisa su README si esto cambia
+      injectThemeJs = false;
       replaceColors = true;
       sidebarConfig = true;
       homeConfig = true;
       overwriteAssets = false;
       additonalCss = "";
     };
-
-    # Colores dinámicos generados por matugen (ver spicetify-colors.nix.template).
-    # colorScheme se vuelve "custom" automáticamente cuando esto no está vacío.
-    customColorScheme = import /home/oozenix/.cache/matugen/spicetify-colors.nix;
 
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
